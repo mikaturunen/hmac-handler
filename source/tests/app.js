e3956f03878fa1bc3e930b31b2c61c95e42e4f21
Mika Turunen <mika.turunen@ymail.com>
2017-05-28 18:42:29 +0300

    Fix test.

--- source/tests/app.js
+++ source/tests/app.js
@@ -3,10 +3,19 @@ const hmac = require('../app')
 
 describe('Calculate hmac', _ => {
   describe('using library defaults', _ => {
-    it('for ..."', _ => {
-      const hmacValidator = hmac()
-      console.log(hmacValidator.calculate([5057619800, "420e577f-7458-4d9b-a4db-ed46a8be3602"], "slbaX7reBYScPCuuwQX4fZx7txeyU5MMqywO6o3HNVtnhoheFFiuog2iegi9l5Vc3T19I5QfaJTH8BvNRfIGMLy02h9nleQp8SyQ"))
+    it('hmac should be valid with properties [8583802900, "420e577f-7458-4d9b-a4db-ed46a8be3602"] and secret "slbaX7reBYScPCuuwQX4fZx7txeyU5MMqywO6o3HNVtnhoheFFiuog2iegi9l5Vc3T19I5QfaJTH8BvNRfIGMLy02h9nleQp8SyQ"', done => {
       
+      const hmacValidator = hmac()
+      const properties = [8583802900, '420e577f-7458-4d9b-a4db-ed46a8be3602']
+      const secret = 'slbaX7reBYScPCuuwQX4fZx7txeyU5MMqywO6o3HNVtnhoheFFiuog2iegi9l5Vc3T19I5QfaJTH8BvNRfIGMLy02h9nleQp8SyQ'
+      const shouldBeResult = '80af905e38c158dc943c0e12e44ab2f4c18363d27934446f6765515424529bbc'
+      
+      assert.equal(
+        hmacValidator.calculate(properties, secret),
+        '80af905e38c158dc943c0e12e44ab2f4c18363d27934446f6765515424529bbc'
+      )
+      
+      done()
     })
   })
 });
